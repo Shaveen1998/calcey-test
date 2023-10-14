873d8cadc41d79b84d4762ca6c53f1009e8b9331
iamshaveenvf <shaveenfernando1@gmail.com>
2023-10-15 00:18:49 +0530

    initial commit

---
+++ Car.h
@@ -0,0 +1,12 @@
+#ifndef CAR_H
+#define CAR_H
+#include "libraries.h"
+
+class Car
+{
+public:
+    string model;
+    void printDetails();
+};
+
+#endif
---
+++ libraries.h
@@ -0,0 +1,7 @@
+#ifndef LIBRARIES_H
+#define LIBRARIES_H
+
+#include <iostream>
+using namespace std;
+#include <string>
+#endif
